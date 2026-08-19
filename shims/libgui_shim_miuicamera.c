9e1ffb5e5adac686bc9e20b203b7f755c3284864
NSR-XINE <nasironfire07@gmail.com>
2026-08-19 22:03:13 +0400

    uke-camera: Add GraphicBuffer lockImageFromBuffer shim symbol

--- shims/libgui_shim_miuicamera.c
+++ shims/libgui_shim_miuicamera.c
@@ -4,6 +4,18 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdint.h>
+
 void _ZN7android18BnProducerListener16onBufferDetachedEi() {
     return;
+}
+
+int32_t _ZN7android19lockImageFromBufferENS_2spINS_13GraphicBufferEEEjRKNS_4RectEiPNS_11CpuConsumer12LockedBufferE(
+        void *buffer, uint32_t usage, void *rect, int fence, void *outBuffer) {
+    (void)buffer;
+    (void)usage;
+    (void)rect;
+    (void)fence;
+    (void)outBuffer;
+    return 0;
 }
