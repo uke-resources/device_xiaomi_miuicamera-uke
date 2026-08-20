4d8d178e977bec5ec9d7ee5f01db780c61ca5c9a
NSR-XINE <nasironfire07@gmail.com>
2026-08-20 13:58:06 +0400

    uke-camera: Register soong namespace and fix dependencies

--- device.mk
+++ device.mk
@@ -4,8 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+MIUICAMERA_PATH := device/xiaomi/uke-camera
+
 # Camera
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
+
+PRODUCT_SOONG_NAMESPACES += \
+    $(MIUICAMERA_PATH)
 
 PRODUCT_PACKAGES += \
     MiuiCamera \
@@ -16,6 +21,6 @@ PRODUCT_PACKAGES += \
     libgui_shim_miuicamera
 
 PRODUCT_COPY_FILES += \
-    device/xiaomi/uke-camera/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-miuicamera.xml \
-    device/xiaomi/uke-camera/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml \
-    device/xiaomi/uke-camera/configs/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-xiaomi.txt
+    $(MIUICAMERA_PATH)/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-miuicamera.xml \
+    $(MIUICAMERA_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml \
+    $(MIUICAMERA_PATH)/configs/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-xiaomi.txt
