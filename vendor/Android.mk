57ef8750c96cd964a8447d85f68fb41bda9679e7
NSR-XINE <nasironfire07@gmail.com>
2026-08-21 23:18:19 +0400

    miuicamera-uke: Split MiuiCamera.apk into chunks

--- vendor/Android.mk
+++ vendor/Android.mk
@@ -1,5 +1,11 @@
-#
 # Automatically generated file. DO NOT MODIFY
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter uke,$(TARGET_DEVICE)),)
+
+# Reassemble split MiuiCamera.apk before build
+$(shell cat $(LOCAL_PATH)/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk.* > $(LOCAL_PATH)/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk 2>/dev/null)
+
+endif
